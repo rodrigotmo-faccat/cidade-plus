c816d74348d6c626af7472a039573673ec7e42c2
rodrigotmo <rodrigotmo@sou.faccat.br>
2024-11-27 21:45:33 -0300

    implementada a geração de um documento PDF com as demandas listadas.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,10 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
+import open_document
+import open_file_mac
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
 
@@ -18,7 +21,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  OpenDocumentPlugin.register(with: registry.registrar(forPlugin: "OpenDocumentPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
